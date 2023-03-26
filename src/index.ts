b2af72028e56320c5af1e0550a1ba9a80bd14fc5
Isa Askin <isa33649@gmail.com>
2023-03-26 22:38:35 +0200

    Made some progress

--- src/index.ts
+++ src/index.ts
@@ -6,15 +6,12 @@ import { parse } from "./plantuml"
 import * as types from "./types"
 const { getTreemap } = require('treemap-squarify')
 
-const arraySmooth = require('array-smooth')
-
 
 const runBuml = (encodedPlantUMLCode: string, width: number, height: number) => {
     
     const decodedData: string = plantumlEncoder.decode(encodedPlantUMLCode)
 
     const parsedData: types.UML[] = parse(decodedData)
-
 
     let filtered: types.Class[] & types.Enum[] & types.Interface[] = []
     
@@ -26,19 +23,20 @@ const runBuml = (encodedPlantUMLCode: string, width: number, height: number) => 
 
     let result = filtered.map((element: types.Class | types.Enum | types.Interface)=> (
         {
-            value: Math.floor(Math.sqrt(element.members.length)),
+            value: Math.floor(Math.sqrt(element.members.length + 1)),
             data: element,
             type: element instanceof types.Class ? "class" :
                   element instanceof types.Enum ? "enum" :
-                  element instanceof types.Interface ? "interface" : ""
+                  element instanceof types.Interface ? "interface" : "",
+            members: element.members
         }
     ))
 
     return {
         tiles: getTreemap({
             data: result,
-            width: 500,
-            height: 300
+            width: width,
+            height: height
         }),
         relationships: parsedData[0].elements.filter(element => {
             return element instanceof types.Relationship ? true : false     
